6e07903a107cbbfa3ed78095dd579c4183d1f890
Daniel Diaz <daniel.diaz201355@hotmail.com>
2020-12-31 10:15:50 -0400

    make ball bounce from paddle

--- arkanoid.py
+++ arkanoid.py
@@ -8,7 +8,7 @@ canvasHeight,canvasWidth=500,500
 win = GraphWin("Arkanoid",canvasWidth,canvasHeight)
 win.setBackground(color_rgb(63, 195, 182))
 win.setCoords(0,0,500,500)
-
+ballRadius=25
 
 
 # Paddle
@@ -27,21 +27,40 @@ def movingPaddle(paddle,clickPoint):
 # Bricks
 # gameplay
 def gameplay():
+
     paddle=Rectangle(Point(200,20),Point(300,40))
     paddleCenter = 50
     paddle.draw(win)
+
+    ball=Circle(Point(150,200),ballRadius)
+    ball.draw(win)
+    ballXspeed=7
+    ballYspeed=7
+    
     gameOver=False
 
+
     while True:
+
+        # Making ball bounce un the boundaries of the page
+        if (ball.getCenter().getX()+ballRadius) >= canvasWidth or (ball.getCenter().getX()-ballRadius) <= 0:
+            ballXspeed = ballXspeed * -1
+        if (ball.getCenter().getY()+ballRadius) >= canvasHeight or (ball.getCenter().getY()-ballRadius) <= 0:
+            ballYspeed = ballYspeed * -1
         
+        # movement of ball
+        ball.move(ballXspeed, ballYspeed)
+
+        # movement of the paddle left or right with keys
         key = win.checkKey()
         if key == "Right":
             if 450!=paddle.getCenter().getX():
                 paddle.move(20.0,0.0)
-
         if key == "Left":
             if 50!=paddle.getCenter().getX():
                 paddle.move(-20.0,0.0)
+
+                
             
             
 
