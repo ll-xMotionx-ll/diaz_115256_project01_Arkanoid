ee9aace0cba46751281501f8fe28fa0d9c1ae718
Daniel Diaz <daniel.diaz201355@hotmail.com>
2020-12-31 07:31:46 -0400

    create function of paddle

--- arkanoid.py
+++ arkanoid.py
@@ -3,21 +3,22 @@ from graphics import *
 #TK_SILENCE_DEPRECATION=1
 
 # Canvas
+canvasHeight,canvasWidth=500,500
 
-#draw(win)
+win = GraphWin("Arkanoid",canvasWidth,canvasHeight)
+win.setBackground(color_rgb(63, 195, 182))
+
+win.getMouse()
+
 # Paddle
+
 # Bounce ball
 # Bricks
 # gameplay
 
 
 def main():
-    canvasHeight,canvasWidth=500,500
-
-    win = GraphWin("Arkanoid",canvasWidth,canvasHeight)
-    win.setBackground(color_rgb(63, 195, 182))
     
-    win.getMouse()
     #gameplay()
 
 main()
