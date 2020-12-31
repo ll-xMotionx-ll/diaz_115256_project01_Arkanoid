4772d2478fb2aec4e784807097a3a64cea4cad7f
Daniel Diaz <daniel.diaz201355@hotmail.com>
2020-12-31 10:14:20 -0400

    movement of ball

--- arkanoid.py
+++ arkanoid.py
@@ -7,18 +7,49 @@ canvasHeight,canvasWidth=500,500
 
 win = GraphWin("Arkanoid",canvasWidth,canvasHeight)
 win.setBackground(color_rgb(63, 195, 182))
+win.setCoords(0,0,500,500)
 
-win.getMouse()
+
 
 # Paddle
+#paddle=Rectangle(Point(200,20),Point(300,40))
+#paddle.draw(win)
+
+def movingPaddle(paddle,clickPoint):
+    
+    while True:
+        #if(clickPoint):
+        paddle.move(clickPoint.getX(),30)
+            #paddle.draw(win)
+    
 
 # Bounce ball
 # Bricks
 # gameplay
+def gameplay():
+    paddle=Rectangle(Point(200,20),Point(300,40))
+    paddleCenter = 50
+    paddle.draw(win)
+    gameOver=False
+
+    while True:
+        
+        key = win.checkKey()
+        if key == "Right":
+            if 450!=paddle.getCenter().getX():
+                paddle.move(20.0,0.0)
+
+        if key == "Left":
+            if 50!=paddle.getCenter().getX():
+                paddle.move(-20.0,0.0)
+            
+            
+
 
 
 def main():
     
-    #gameplay()
+    gameplay()
+    win.getMouse()
 
 main()
